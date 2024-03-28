efaaada88d60e7bcf5392bf515c3f2367a44f0dc
Max Hopf <maxhopf@pm.me>
2024-03-28 08:44:56 +0100

    Optimize for search engine

--- script.js
+++ script.js
@@ -1,20 +1,2 @@
 
 const thisYear = document.querySelector("#thisYear").textContent = new Date().getFullYear();
-
-/*
-const phoneNumber = document.querySelector(`#phone-number`);
-
-phoneNumber.addEventListener('click', copyTextContent);
-phoneNumber.addEventListener('mouseover', showSignifier);
-
-function showSignifier() {
-    const tooltip = document.querySelector('#myTooltip');
-    tooltip.textContent = "Kopieren";
-}
-
-function copyTextContent() {
-    const tooltip = document.querySelector('#myTooltip');
-    tooltip.textContent = "Kopiert";
-    navigator.clipboard.writeText(phoneNumber.textContent.replace(/\s/g, ''));
-}
-*/
